db6c35707ff787d69e57f51d0f93568e23619ecf
HguitarH <hanan.hazani@gmail.com>
2021-05-22 18:47:19 +0300

    MoveZeroes

---
+++ LeetCodeProblems/MoveZeroes.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeProblems
+{
+    public class MoveZeroes
+    {
+        public void Solve(int[] nums)
+        {
+            var zeros = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    zeros++;
+                }
+                else if (zeros > 0)
+                {
+                    nums[i - zeros] = nums[i];
+                    nums[i] = 0;
+                }
+            }
+        }
+    }
+}
