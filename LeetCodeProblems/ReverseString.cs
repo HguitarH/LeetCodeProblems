620d0b96c5e887d98e1da1a339fa5c2fd96c0ca1
HguitarH <hanan.hazani@gmail.com>
2021-05-22 18:48:28 +0300

    ReverseString

---
+++ LeetCodeProblems/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class ReverseString
+    {
+        public void Solve(char[] s)
+        {
+            char temp;
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                temp = s[i];
+                s[i] = s[s.Length - 1 - i];
+                s[s.Length - 1 - i] = temp;
+            }
+        }
+    }
+}
