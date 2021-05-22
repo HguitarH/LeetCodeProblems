b11484127ee23b2ceb7d32e13f11a23182caf7d9
HguitarH <hanan.hazani@gmail.com>
2021-05-22 18:38:17 +0300

    FirstBadVersion

---
+++ LeetCodeProblems/FirstBadVersion.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeProblems
+{
+    public class FirstBadVersion
+    {
+        // given API
+        private bool IsBadVersion(int _) => true;
+
+
+        public int Solve(int n)
+        {
+            int left = 1;
+            int right = n;
+
+            while (left < right)
+            {
+                int p = right / 2 + left / 2;
+                if (IsBadVersion(p))
+                    right = p;
+                else
+                    left = p + 1;
+            }
+
+            return left;
+        }
+    }
+}
