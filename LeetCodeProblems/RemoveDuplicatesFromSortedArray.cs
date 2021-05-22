4fbc643bb4bb7b3478a6d3fa42397b35de6161eb
HguitarH <hanan.hazani@gmail.com>
2021-05-22 18:45:58 +0300

    RemoveDuplicatesFromSortedArray

---
+++ LeetCodeProblems/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeProblems
+{
+    public class RemoveDuplicatesFromSortedArray
+    {
+        public int Solve(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+            var size = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[size - 1] < nums[i])
+                {
+                    nums[size] = nums[i];
+                    size++;
+                }
+            }
+
+            return size;
+        }
+    }
+}
