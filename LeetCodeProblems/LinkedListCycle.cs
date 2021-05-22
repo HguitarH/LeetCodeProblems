ca4e11b3921cbd5e3409bf60c66053f72dafee04
HguitarH <hanan.hazani@gmail.com>
2021-05-22 18:41:09 +0300

    LinkedListCycle

---
+++ LeetCodeProblems/LinkedListCycle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCodeProblems
+{
+    public class LinkedListCycle
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+
+        public bool Solve(ListNode head)
+        {
+            while (head != null)
+            {
+                if (head.val == Int32.MinValue)
+                    return true;
+                head.val = Int32.MinValue;
+                head = head.next;
+            }
+            return false;
+        }
+    }
+}
