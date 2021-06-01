cb9df3ca7618090d027abd4bad0e6f91f3f52099
HguitarH <hanan.hazani@gmail.com>
2021-06-01 15:21:08 +0300

    HouseRobber & MinStack

---
+++ LeetCodeProblems/HouseRobber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCodeProblems
+{
+    public class HouseRobber
+    {
+        int[] sums;
+        public int Solve(int[] nums)
+        {
+            if (nums.Length == 1)
+                return nums[0];
+
+            sums = new int[nums.Length];
+            sums[0] = nums[0];
+            sums[1] = Math.Max(sums[0], nums[1]);
+            for (int i = 2; i < nums.Length; i++)
+            {
+                sums[i] = Math.Max(sums[i - 1], sums[i - 2] + nums[i]);
+            }
+            return sums[sums.Length - 1];
+        }
+    }
+}
---
+++ LeetCodeProblems/MinStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeProblems
+{
+    public class MinStack
+    {
+        private Stack<(int, int)> data;
+        private int min;
+
+        public MinStack()
+        {
+            data = new Stack<(int, int)>();
+            min = int.MaxValue;
+        }
+
+        public void Push(int val)
+        {
+            data.Push((val, min));
+            min = Math.Min(val, min);
+        }
+
+        public void Pop()
+        {
+            var elem = data.Peek();
+            min = elem.Item2;
+            data.Pop();
+        }
+
+        public int Top()
+        {
+            return data.Peek().Item1;
+        }
+
+        public int GetMin()
+        {
+            return min;
+        }
+    }
+}
